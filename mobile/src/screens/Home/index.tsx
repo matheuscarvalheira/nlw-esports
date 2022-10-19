eb927aee92c205d48d04186a72022fa17d4c5c88
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-10-19 17:00:32 -0300

    pulling games through the backend

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,11 +1,22 @@
-import { Image, View, FlatList } from "react-native";
-import logoImg from "../../assets/logo-nlw-esports.png";
-import { Heading } from "../../components/Heading";
-import { GameCard } from "../../components/GameCard";
-import { GAMES } from "../../utils/games";
 import { styles } from "./styles";
+import {  useEffect, useState } from 'react';
+import { Image, View, FlatList } from "react-native";
+import { Heading } from "../../components/Heading";
+import { GameCard, GameCardProps } from "../../components/GameCard";
+
+import logoImg from "../../assets/logo-nlw-esports.png";
 
 export function Home() {
+
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+    .then(response => response.json())
+    .then(data => setGames(data));
+  }, []);
+
+
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
@@ -16,7 +27,7 @@ export function Home() {
       />
 
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <GameCard data={item} />}
         showsHorizontalScrollIndicator={false}
