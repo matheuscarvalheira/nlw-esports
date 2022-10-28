c4a47192c8308575fff73af40f132d4854d7ca18
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-10-28 20:02:43 -0300

    getting all games for select

--- web/src/components/CreateAdModal.tsx
+++ web/src/components/CreateAdModal.tsx
@@ -1,10 +1,28 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import * as Checkbox from '@radix-ui/react-checkbox';
+import * as Checkbox from "@radix-ui/react-checkbox";
+import * as ToggleGroup from "@radix-ui/react-toggle-group";
 
+import { useEffect, useState } from "react";
 import { Check, GameController } from "phosphor-react";
+
 import Input from "./Form/Input";
 
+interface Game {
+  id: string;
+  title: string;
+}
+
 export function CreateAdModal() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/games").then((response) =>
+      response.json().then((data) => {
+        setGames(data);
+      })
+    );
+  }, []);
+
   return (
     <Dialog.Portal>
       <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
@@ -19,7 +37,21 @@ export function CreateAdModal() {
             <label htmlFor="game" className="font-semibold">
               Qual o game?
             </label>
-            <Input id="game" placeholder="Selecione o game que deseja jogar" />
+            <select
+              id="game"
+              className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 appearance-none"
+            >
+              <option disabled selected value="">
+                Selecione o game que deseja jogar
+              </option>
+              {games.map((game) => {
+                return (
+                  <option key={game.id} value={game.id}>
+                    {game.title}
+                  </option>
+                );
+              })}
+            </select>
           </div>
 
           <div className="flex flex-col gap-2">
@@ -47,30 +79,61 @@ export function CreateAdModal() {
             <div className="flex flex-col gap-2">
               <label htmlFor="weekDays">Quando costuma jogar?</label>
 
-              <div className="grid grid-cols-4 gap-4">
-                <button title="Domingo" className="w-8 h-8 rounded bg-zinc-900">
+              <ToggleGroup.Root
+                type="multiple"
+                className="grid grid-cols-4 gap-4"
+              >
+                <ToggleGroup.Item
+                  value="0"
+                  title="Domingo"
+                  className="w-8 h-8 rounded bg-zinc-900"
+                >
                   D
-                </button>
+                </ToggleGroup.Item>
 
-                <button title="Segunda" className="w-8 h-8 rounded bg-zinc-900">
+                <ToggleGroup.Item
+                  value="1"
+                  title="Segunda"
+                  className="w-8 h-8 rounded bg-zinc-900"
+                >
                   S
-                </button>
-                <button title="Terça" className="w-8 h-8 rounded bg-zinc-900">
+                </ToggleGroup.Item>
+                <ToggleGroup.Item
+                  value="2"
+                  title="Terça"
+                  className="w-8 h-8 rounded bg-zinc-900"
+                >
                   T
-                </button>
-                <button title="Quarta" className="w-8 h-8 rounded bg-zinc-900">
+                </ToggleGroup.Item>
+                <ToggleGroup.Item
+                  value="3"
+                  title="Quarta"
+                  className="w-8 h-8 rounded bg-zinc-900"
+                >
                   Q
-                </button>
-                <button title="Quinta" className="w-8 h-8 rounded bg-zinc-900">
+                </ToggleGroup.Item>
+                <ToggleGroup.Item
+                  value="4"
+                  title="Quinta"
+                  className="w-8 h-8 rounded bg-zinc-900"
+                >
                   Q
-                </button>
-                <button title="Sexta" className="w-8 h-8 rounded bg-zinc-900">
+                </ToggleGroup.Item>
+                <ToggleGroup.Item
+                  value="5"
+                  title="Sexta"
+                  className="w-8 h-8 rounded bg-zinc-900"
+                >
                   S
-                </button>
-                <button title="Sábado" className="w-8 h-8 rounded bg-zinc-900">
+                </ToggleGroup.Item>
+                <ToggleGroup.Item
+                  value="6"
+                  title="Sábado"
+                  className="w-8 h-8 rounded bg-zinc-900"
+                >
                   S
-                </button>
-              </div>
+                </ToggleGroup.Item>
+              </ToggleGroup.Root>
             </div>
             <div className="flex flex-col gap-2 flex-1">
               <label htmlFor="hourStart">Qual horário do dia?</label>
@@ -91,13 +154,17 @@ export function CreateAdModal() {
           </div>
 
           <footer className="mt-4 flex justify-end gap-4">
-            <Dialog.Close className="bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600">
+            <Dialog.Close
+              className="bg-zinc-500 px-5 h-12 rounded-md font-semibold
+             hover:bg-zinc-600"
+            >
               Cancelar
             </Dialog.Close>
 
             <button
               type="submit"
-              className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600"
+              className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex 
+              items-center gap-3 hover:bg-violet-600"
             >
               <GameController size={24} />
               Encontrar Duo
