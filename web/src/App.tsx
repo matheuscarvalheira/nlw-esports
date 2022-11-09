c3c0c19044e059f8befc1e846c20138296d3d0c3
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-11-09 16:11:07 -0300

    using axios

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import { GameBanner } from "./components/GameBanner";
 import { CreateAdBanner } from "./components/CreateAdBanner";
 import { CreateAdModal } from "./components/CreateAdModal";
+import axios from "axios";
 
 interface Game {
   id: string;
@@ -21,11 +22,9 @@ function App() {
   const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:3333/games").then((response) =>
-      response.json().then((data) => {
-        setGames(data);
+    axios("http://localhost:3333/games").then((response) => {
+        setGames(response.data);
       })
-    );
   }, []);
 
   return (
