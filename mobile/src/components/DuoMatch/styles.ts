7984a23871a6d7436895a6a61558cd60bc4d874a
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-11-16 19:17:22 -0300

    styling the discord button

--- mobile/src/components/DuoMatch/styles.ts
+++ mobile/src/components/DuoMatch/styles.ts
@@ -19,6 +19,23 @@ export const styles = StyleSheet.create({
     alignSelf: 'flex-end',
     margin: 16
   },
+  label: {
+    color: THEME.COLORS.TEXT,
+    fontSize: THEME.FONT_SIZE.MD,
+    fontFamily: THEME.FONT_FAMILY.SEMI_BOLD,
+    marginTop: 24,
+    marginBottom: 8,
+  },
+  discordButton: {
+    width: 231,
+    height:48,
+    backgroundColor: THEME.COLORS.BACKGROUND_900,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+    marginBottom: 32,
+
+  },
   discord: {
     color: THEME.COLORS.TEXT,
     fontSize: THEME.FONT_SIZE.MD,
